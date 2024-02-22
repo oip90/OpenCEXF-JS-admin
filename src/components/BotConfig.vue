c258e87e7bae9aed3e36e3c091de6914ea458ea8
oip90 <oip90@mail.ru>
2024-02-22 19:48:06 +0300

    botconfig
    add more then 5 pair select

--- src/components/BotConfig.vue
+++ src/components/BotConfig.vue
@@ -259,17 +259,39 @@ const fetchUsers = async (searchValue) => {
 
 const pairs = ref([])
 
-const fetchPairs = async () => {
-  try {
-    const response = await axios.get(`${apiKey}core/pair/`)
-    response.data.results.map($ => {
-      pairs.value.push({value: $.id, text: $._label})
-    })
-  } catch (error) {
-    console.log(error) 
-  }
-}
+#const fetchPairs = async () => {
+#  try {
+#    const response = await axios.get(`${apiKey}core/pair/`)
+#    response.data.results.map($ => {
+#      pairs.value.push({value: $.id, text: $._label})
+#    })
+#  } catch (error) {
+#    console.log(error) 
+#  }
+#}
 
+
+const fetchPairs = async () => {
+
+  try {
+    let nextUrl = `${apiKey}core/pair/`;
+
+    // Loop until there are no more pages
+    while (nextUrl) {
+      const response = await axios.get(nextUrl);
+      // console.log('Server response:', response.data);
+
+      // Assuming 'results' is the key containing pairs in the response
+      response.data.results.map(pair => {
+        pairs.value.push({ value: pair.id, text: pair._label });
+      });
+
+      // Update the 'nextUrl' for the next page
+      nextUrl = response.data.next;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 const save = async () => {
   let pathSepar = splitAndReplace(removeListSuffix(param.value))
   if(endsWithList(param.value)) 
